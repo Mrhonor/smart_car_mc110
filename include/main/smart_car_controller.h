f0d5c54bf8009c9573880358a2dfb91ae14ef628
mrhonor <619500598@qq.com>
2022-03-09 19:28:21 +0800

    simulation version

--- include/main/smart_car_controller.h
+++ include/main/smart_car_controller.h
@@ -8,6 +8,13 @@
 // cpp
 #include <semaphore.h>
 
+// temp use for simulation
+#include "integrator.h"
+#include "types.h"
+#include "std_msgs/Float64MultiArray.h"
+
+// simulation
+using namespace mpcc;
 
 class smart_car_controller
 {
@@ -21,24 +28,33 @@ private:
     int32 imu_seq;
     int32 encoder_seq;
 
+    // simulation
+    mpcc::Integrator* integrator;
+    State x0;
+    ros::Publisher state_pub_;
+    ros::Publisher reference_path_pub_;
+
+    ros::Subscriber control_sub_;
+    double Ts;
+    int TempSimuEnd;
+
 public:
 
     smart_car_controller(ros::NodeHandle &);
     ~smart_car_controller();
     
+    // simulation
     void trackPublish();
+    void statePublish();
+    void mpccControlCallback(const std_msgs::Float64MultiArrayConstPtr&);
 
 private:
 
     void controllerThreadHandle();
     void SensorInfoPublish(const SRealDataStru& data, const ros::Time &timeStamp);
-    void ImuDataPublish(int16 * gypo, const ros::Time &timeStamp);
+    void ImuDataPublish(const int16 * gypo, const ros::Time &timeStamp);
     void encoderDataPublish(const int16 & vel, const ros::Time &timeStamp);
 
-    ros::Publisher state_pub_;
-    ros::Publisher reference_path_pub_;
-    ros::Publisher track_boundary_left_pub_;
-    ros::Publisher track_boundary_right_pub_;
 
 };
 
